a05689e1ab132295ca40bacdaa0c2e6097685a42
George Leyva <leyvageorge@hotmail.com>
2026-05-12 23:17:13 -0700

    Had git file to the wrong folder

---
+++ CritThink1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string fName = "Jorge";
+    string lName = "Leyva";
+    string streetAddress = "123 Main Street";
+    string city = "Anytown";
+    int zipCode = 54321;
+    cout << fName << " " << lName << endl;
+    cout << streetAddress << endl;
+    cout << city << ", " << zipCode << endl;
+    return 0;
+}
